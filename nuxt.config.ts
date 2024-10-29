34d812ae13e238a2a87388969fa7e823444da46d
Can Stand <canstand@outlook.com>
2024-10-29 12:15:06 +0800

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,33 +12,8 @@ export default defineNuxtConfig({
     'radix-vue/nuxt',
   ],
 
-  experimental: {
-    // when using generate, payload js assets included in sw precache manifest
-    // but missing on offline, disabling extraction it until fixed
-    payloadExtraction: false,
-    renderJsonPayloads: true,
-    typedPages: true,
-  },
-
-  css: [
-    '@unocss/reset/tailwind.css',
-  ],
-
-  colorMode: {
-    classSuffix: '',
-  },
-
-  nitro: {
-    esbuild: {
-      options: {
-        target: 'esnext',
-      },
-    },
-    prerender: {
-      crawlLinks: false,
-      routes: ['/'],
-      ignore: ['/hi'],
-    },
+  devtools: {
+    enabled: true,
   },
 
   app: {
@@ -59,26 +34,49 @@ export default defineNuxtConfig({
     },
   },
 
-  pwa,
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
 
-  devtools: {
-    enabled: true,
-  },
-
-  features: {
-    // For UnoCSS
-    inlineStyles: false,
-  },
-
-  eslint: {
-    config: {
-      standalone: false,
-    },
+  colorMode: {
+    classSuffix: '',
   },
 
   future: {
     compatibilityVersion: 4,
   },
 
-  compatibilityDate: '2024-09-09',
+  experimental: {
+    // when using generate, payload js assets included in sw precache manifest
+    // but missing on offline, disabling extraction it until fixed
+    payloadExtraction: false,
+    renderJsonPayloads: true,
+    typedPages: true,
+  },
+
+  compatibilityDate: '2024-10-29',
+
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+    prerender: {
+      crawlLinks: false,
+      routes: ['/'],
+      ignore: ['/hi'],
+    },
+  },
+
+  eslint: {
+    config: {
+      standalone: false,
+      nuxt: {
+        sortConfigKeys: true,
+      },
+    },
+  },
+
+  pwa,
 })
