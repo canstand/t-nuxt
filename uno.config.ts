400844f657bb92e680c1af31a039c51c164a6166
Can Stand <canstand@outlook.com>
2024-12-10 17:02:12 +0800

    chore: update deps

--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,12 @@
 import { h } from '@unocss/preset-mini/utils'
+import { createLocalFontProcessor } from '@unocss/preset-web-fonts/local'
 import {
   defineConfig,
   presetAttributify,
   presetIcons,
   presetTypography,
   presetUno,
+  presetWebFonts,
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
@@ -97,6 +99,15 @@ export default defineConfig({
       scale: 1.2,
     }),
     presetTypography(),
+    presetWebFonts({
+      provider: 'bunny',
+      fonts: {
+        sans: 'DM Sans',
+        serif: 'DM Serif Display',
+        mono: 'DM Mono',
+      },
+      processors: createLocalFontProcessor(),
+    }),
   ],
   transformers: [
     transformerDirectives(),
